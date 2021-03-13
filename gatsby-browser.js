981ceedef7b010e78b7c25d94550838b1b192890
muhrusdi <mhmrofficial@gmail.com>
2021-03-14 01:47:56 +0800

    update gatsby image

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,3 +1,6 @@
 import "./src/styles/globals.css"
 
+/**
+ * For enable the apollo client, you should uncomment the code below
+ */
 // export { wrapRootElement } from './src/apollo/wrap-root-element';
