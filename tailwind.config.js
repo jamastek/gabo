585eba8f88a2e5407b32e9b84bc0474ef9051fa0
muhrusdi <mhmrofficial@gmail.com>
2022-03-19 02:27:09 +0800

    remove jit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,6 @@
 const colors = require("tailwindcss/colors")
 
 module.exports = {
-  mode: "jit",
-  future: {
-    // removeDeprecatedGapUtilities: true,
-    // purgeLayersByDefault: true,
-  },
   purge: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     container: {
